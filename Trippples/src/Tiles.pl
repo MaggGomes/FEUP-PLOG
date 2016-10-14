d8af1f52d47b1516dd3df5ffa2471fd261b1d73c
MaggGomes <magg.slb@gmail.cm>
2016-10-14 15:18:33 +0100

    some tiles added

--- Trippples/src/Tiles.pl
+++ Trippples/src/Tiles.pl
@@ -1,10 +1,196 @@
 % Game tiles
-translate(0, 1):- write('A11').
-translate(0, 2):- write('A22').
-translate(0, 3):- write('A33').
-translate(1, 1):- write('B11').
-translate(1, 2):- write('B22').
-translate(1, 3):- write('B33').
-translate(2, 1):- write('C11').
-translate(2, 2):- write('C11').
-translate(2, 3):- write('C11').
+
+% Empty tile
+translate(0, 1):- write('   ').
+translate(0, 2):- write('   ').
+translate(0, 3):- write('   ').
+
+% Starting and finish markers
+translate(1, 1):- write(' - ').
+translate(1, 2):- write('| |').
+translate(1, 3):- write(' - ').
+translate(2, 1):- write(' - ').
+translate(2, 2):- write('|#|').
+translate(2, 3):- write(' - ').
+translate(3, 1):- write('   ').
+translate(3, 2):- write('( )').
+translate(3, 3):- write('   ').
+translate(4, 1):- write('   ').
+translate(4, 2):- write('(#)').
+translate(4, 3):- write('   ').
+
+% Directional tiles
+translate(5, 1):- write('_|_').
+translate(5, 2):- write('   ').
+translate(5, 3):- write('   ').
+translate(6, 1):- write('   ').
+translate(6, 2):- write('_ _').
+translate(6, 3):- write(' | ').
+translate(7, 1):- write(' | ').
+translate(7, 2):- write('-- ').
+translate(7, 3):- write(' | ').
+translate(8, 1):- write(' | ').
+translate(8, 2):- write(' --').
+translate(8, 3):- write(' | ').
+translate(9, 1):- write('   ').
+translate(9, 2):- write('   ').
+translate(9, 3):- write('/|\\').
+translate(10, 1):- write('  /').
+translate(10, 2):- write(' --').
+translate(10, 3):- write('  \\').
+translate(11, 1):- write('\\  ').
+translate(11, 2):- write('-- ').
+translate(11, 3):- write('/  ').
+translate(12, 1):- write('\\|/').
+translate(12, 2):- write('   ').
+translate(12, 3):- write('   ').
+
+
+
+% Falta fazer estas tiles
+translate(13, 1):- write('\\| ').
+translate(13, 2):- write('-  ').
+translate(13, 3):- write('   ').
+
+
+translate(14, 1):- write('\\|/').
+translate(14, 2):- write('   ').
+translate(14, 3):- write('   ').
+translate(15, 1):- write('\\|/').
+translate(15, 2):- write('   ').
+translate(15, 3):- write('   ').
+translate(16, 1):- write('\\|/').
+translate(16, 2):- write('   ').
+translate(16, 3):- write('   ').
+translate(17, 1):- write('\\|/').
+translate(17, 2):- write('   ').
+translate(17, 3):- write('   ').
+translate(18, 1):- write('\\|/').
+translate(18, 2):- write('   ').
+translate(18, 3):- write('   ').
+translate(19, 1):- write('\\|/').
+translate(19, 2):- write('   ').
+translate(19, 3):- write('   ').
+translate(20, 1):- write('\\|/').
+translate(20, 2):- write('   ').
+translate(20, 3):- write('   ').
+translate(21, 1):- write('\\|/').
+translate(21, 2):- write('   ').
+translate(21, 3):- write('   ').
+translate(22, 1):- write('\\|/').
+translate(22, 2):- write('   ').
+translate(22, 3):- write('   ').
+translate(23, 1):- write('   ').
+translate(23, 2):- write('   ').
+translate(23, 3):- write('\\|/').
+translate(24, 1):- write('   ').
+translate(24, 2):- write('   ').
+translate(24, 3):- write('\\|/').
+translate(25, 1):- write('   ').
+translate(25, 2):- write('   ').
+translate(25, 3):- write('\\|/').
+translate(26, 1):- write('   ').
+translate(26, 2):- write('   ').
+translate(26, 3):- write('\\|/').
+translate(27, 1):- write('   ').
+translate(27, 2):- write('   ').
+translate(27, 3):- write('\\|/').
+translate(28, 1):- write('   ').
+translate(28, 2):- write('   ').
+translate(28, 3):- write('   ').
+translate(29, 1):- write('\\|/').
+translate(29, 2):- write('   ').
+translate(29, 3):- write('   ').
+translate(30, 1):- write('\\|/').
+translate(30, 2):- write('   ').
+translate(30, 3):- write('   ').
+translate(31, 1):- write('\\|/').
+translate(31, 2):- write('   ').
+translate(31, 3):- write('   ').
+translate(32, 1):- write('\\|/').
+translate(32, 2):- write('   ').
+translate(32, 3):- write('   ').
+translate(33, 1):- write('\\|/').
+translate(33, 2):- write('   ').
+translate(33, 3):- write('   ').
+translate(34, 1):- write('\\|/').
+translate(34, 2):- write('   ').
+translate(34, 3):- write('   ').
+translate(35, 1):- write('\\|/').
+translate(35, 2):- write('   ').
+translate(35, 3):- write('   ').
+translate(36, 1):- write('\\|/').
+translate(36, 2):- write('   ').
+translate(36, 3):- write('   ').
+translate(37, 1):- write('\\|/').
+translate(37, 2):- write('   ').
+translate(37, 3):- write('   ').
+translate(38, 1):- write('\\|/').
+translate(38, 2):- write('   ').
+translate(38, 3):- write('   ').
+translate(39, 1):- write('\\|/').
+translate(39, 2):- write('   ').
+translate(39, 3):- write('   ').
+translate(40, 1):- write('\\|/').
+translate(40, 2):- write('   ').
+translate(40, 3):- write('   ').
+translate(41, 1):- write('\\|/').
+translate(41, 2):- write('   ').
+translate(41, 3):- write('   ').
+translate(42, 1):- write('\\|/').
+translate(42, 2):- write('   ').
+translate(42, 3):- write('   ').
+translate(43, 1):- write('   ').
+translate(43, 2):- write('\\|/').
+translate(43, 3):- write('   ').
+translate(44, 1):- write('   ').
+translate(44, 2):- write('\\|/').
+translate(44, 3):- write('   ').
+translate(45, 1):- write('   ').
+translate(45, 2):- write('\\|/').
+translate(45, 3):- write('   ').
+translate(46, 1):- write('   ').
+translate(46, 2):- write('\\|/').
+translate(46, 3):- write('   ').
+translate(47, 1):- write('   ').
+translate(47, 2):- write('\\|/').
+translate(47, 3):- write('   ').
+translate(48, 1):- write('   ').
+translate(48, 2):- write('\\|/').
+translate(48, 3):- write('   ').
+translate(49, 1):- write('   ').
+translate(49, 2):- write('\\|/').
+translate(49, 3):- write('   ').
+translate(50, 1):- write('   ').
+translate(50, 2):- write('   ').
+translate(50, 3):- write('\\|/').
+translate(51, 1):- write('   ').
+translate(51, 2):- write('   ').
+translate(51, 3):- write('\\|/').
+translate(52, 1):- write('   ').
+translate(52, 2):- write('   ').
+translate(52, 3):- write('\\|/').
+translate(53, 1):- write('   ').
+translate(53, 2):- write('   ').
+translate(53, 3):- write('\\|/').
+translate(54, 1):- write('   ').
+translate(54, 2):- write('   ').
+translate(54, 3):- write('\\|/').
+translate(55, 1):- write('   ').
+translate(55, 2):- write('   ').
+translate(55, 3):- write('\\|/').
+translate(56, 1):- write('   ').
+translate(56, 2):- write('   ').
+translate(56, 3):- write('\\|/').
+translate(57, 1):- write('   ').
+translate(57, 2):- write('   ').
+translate(57, 3):- write('   ').
+translate(58, 1):- write('\\|/').
+translate(58, 2):- write('   ').
+translate(58, 3):- write('   ').
+translate(59, 1):- write('\\|/').
+translate(59, 2):- write('   ').
+translate(59, 3):- write('   ').
+translate(60, 1):- write('\\|/').
+translate(60, 2):- write('   ').
+translate(60, 3):- write('   ').
--- Trippples/src/main.pl
+++ Trippples/src/main.pl
@@ -11,71 +11,81 @@ run:-
 	board(T), startBoard(T).
 
 % Game board
-board([[1, 0, 0, 0, 0, 0, 0, 0],
-	   [0, 0, 0, 0, 0, 0, 0, 0],
-	   [0, 1, 0, 0, 0, 2, 0, 0],
-	   [0, 0, 0, 0, 0, 0, 0, 0],
-	   [0, 0, 0, 2, 0, 0, 0, 0],
-	   [0, 0, 0, 0, 0, 0, 0, 0],
-	   [0, 0, 0, 0, 0, 0, 0, 0],
-	   [0, 0, 0, 0, 0, 0, 0, 0]]).
+board([[2, 5, 6, 7, 8, 9, 10, 3],
+	   [11, 12, 13, 14, 15, 16, 17, 18],
+	   [19, 20, 21, 22, 23, 24, 25, 26],
+	   [27, 27, 28, 0, 0, 29, 30, 31],
+	   [32, 33, 34, 0, 0, 0, 0, 0],
+	   [0, 0, 0, 0, 12, 0, 7, 0],
+	   [0, 6, 9, 0, 0, 0, 0, 0],
+	   [4, 0, 10, 0, 11, 0, 0, 1]]).
+
+printRowSeparator:-
+	write(' -----------------------------------').
+
+printColSeparator:-
+    write('|').
 
 printTopBorder:-
-	write(' _______________________________'), nl.
-
-printSeparator:-
-	write(' -------------------------------').
+	write(' ___________________________________'), nl,
+    write('|   |   |   |   |   |   |   |   |   |'), nl,
+    write('|   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |'), nl,
+    write('|   |   |   |   |   |   |   |   |   |'), nl,
+    printRowSeparator, nl.
 
 % Prints a line of the game board
 displayRowA([]).
 
 displayRowA([C]):-
 	translate(C, 1),
-	write('|').
+	printColSeparator.
 
 displayRowA([C|Cs]):-
 	translate(C, 1),
-	write('|'),
-	displayRow(Cs).
+	printColSeparator,
+	displayRowA(Cs).
 
 displayRowB([]).
 
 displayRowB([C]):-
 	translate(C, 2),
-	write('|').
+	printColSeparator.
 
 displayRowB([C|Cs]):-
 	translate(C, 2),
-	write('|'),
-	displayRow(Cs).
+	printColSeparator,
+	displayRowB(Cs).
 
 displayRowC([]).
 
 displayRowC([C]):-
 	translate(C, 3),
-	write('|').
+	printColSeparator.
 
 displayRowC([C|Cs]):-
 	translate(C, 3),
-	write('|'),
-	displayRow(Cs).
+	printColSeparator,
+	displayRowC(Cs).
 
 % Prints the game board
 startBoard(T):-
 	printTopBorder,
-	displayBoard(T).
+	displayBoard(T, 0).
 
-displayBoard([]).
+displayBoard([], _).
 
-displayBoard([T|Ts]):-
-	write('|'),
+displayBoard([T|Ts], N):-
+    X is N+1,
+    write('|   |'),
 	displayRowA(T), nl,
-	write('|'),
+    write('| '),
+    write(X),
+    write(' |'),
 	displayRowB(T), nl,
-	write('|'),
+    write('|   |'),    
 	displayRowC(T), nl,
-	printSeparator, nl,
-	displayBoard(Ts).
+	printRowSeparator, nl,
+	displayBoard(Ts, X).
 
 
 
@@ -83,6 +93,7 @@ displayBoard([T|Ts]):-
 
 % Moves the marker of a player to a different tile
 % moveMarker(Player, Tile).
+%gameEnd(G.)
 
 
 
